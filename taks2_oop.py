f5e5d78b8a56792a385a0f283117e3977c7cbebd
kandriadze <96418672+kandriadze@users.noreply.github.com>
2023-02-08 16:20:24 +0400

    Add files via upload

---
+++ taks2_oop.py
@@ -0,0 +1,30 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = float(length)
+        self.width = float(width)
+
+    def get_perimeter(self):
+        return 2 * self.length + 2 * self.width
+
+    def area(self):
+        return self.length * self.width
+
+    def display(self):
+        print(f"length is {self.length} and width is {self.width}")
+        print(f"perimeter is {self.get_perimeter()} and the area is {self.area()}  ")
+
+
+class Parallelepipede(Rectangle):
+
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = float(height)
+
+    def volume(self):
+        print("volume of the parallepipede is " + str(self.length * self.width * self.height))
+
+
+a = Rectangle(5, 6)
+a.display()
+p = Parallelepipede(5, 6, 8)
+p.volume()
